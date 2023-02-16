22082443170e9242472904a27b46e43296c8f617
Laurent Fiack <laurent.fiack@gmail.com>
2023-02-16 11:36:39 +0100

    generated code for WVTBL firmware

---
+++ firmware/wvtbl/Core/Inc/main.h
@@ -0,0 +1,102 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define POT_1_Pin GPIO_PIN_0
+#define POT_1_GPIO_Port GPIOA
+#define POT_2_Pin GPIO_PIN_1
+#define POT_2_GPIO_Port GPIOA
+#define POT_3_Pin GPIO_PIN_2
+#define POT_3_GPIO_Port GPIOA
+#define POT_4_Pin GPIO_PIN_3
+#define POT_4_GPIO_Port GPIOA
+#define DAC_Pin GPIO_PIN_4
+#define DAC_GPIO_Port GPIOA
+#define CV1_Pin GPIO_PIN_5
+#define CV1_GPIO_Port GPIOA
+#define CV2_Pin GPIO_PIN_6
+#define CV2_GPIO_Port GPIOA
+#define CV3_Pin GPIO_PIN_7
+#define CV3_GPIO_Port GPIOA
+#define CV4_Pin GPIO_PIN_0
+#define CV4_GPIO_Port GPIOB
+#define LED_BLUE_Pin GPIO_PIN_8
+#define LED_BLUE_GPIO_Port GPIOA
+#define LED_GREEN_Pin GPIO_PIN_9
+#define LED_GREEN_GPIO_Port GPIOA
+#define LED_RED_Pin GPIO_PIN_10
+#define LED_RED_GPIO_Port GPIOA
+#define ENCODER_B_Pin GPIO_PIN_11
+#define ENCODER_B_GPIO_Port GPIOA
+#define ENCODER_A_Pin GPIO_PIN_12
+#define ENCODER_A_GPIO_Port GPIOA
+#define ENCODER_SWITCH_Pin GPIO_PIN_15
+#define ENCODER_SWITCH_GPIO_Port GPIOA
+#define GPIO2_Pin GPIO_PIN_4
+#define GPIO2_GPIO_Port GPIOB
+#define GPIO1_Pin GPIO_PIN_5
+#define GPIO1_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
